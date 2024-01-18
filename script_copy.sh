53f6a1dd4fabfa4018f14486fa37cfe6907b1b7c
Felipe Infantino <acrylicode.berlin@gmail.com>
2024-01-18 21:27:34 +0100

    script

---
+++ script_copy.sh
@@ -0,0 +1,1 @@
+curl -X POST https://email-service-cnhh.onrender.com/emailService -H "Content-Type: application/json" -d '{"email":"felipe.infantino@hotmail.com","recieveNotification":true,"recievePersonalized":true,"title":"Ripples of Change","date":"2024-01-11","order":[3,2,1,0]}'  
